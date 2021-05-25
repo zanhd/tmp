6d11589e0b157b6016a2e7a54587f5bcdb728db0
Mohd Faizan <48771856+zanhd@users.noreply.github.com>
2021-05-25 20:17:09 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -5,12 +5,12 @@ struct Song
 	int dur;
 };
 
-int BinarySearch(int x, vector<Song> a, int beg, int end)
+int BinarySearch(int x, vector<Song> a, int beg, int end) // a => vector of songs
 {
 	while (beg <= end)
 	{
 		int mid = (beg + end) / 2;
-		if (a[mid].dur == x) return mid;
+		if (a[mid].dur == x) return mid; // compare songs -> duration 
 		if (a[mid].dur < x) beg = mid + 1;
 		else end = mid - 1;
 	}
@@ -21,6 +21,7 @@ vector<int> findSongs(int rideduration, vector<int> songDuration)
 {
 	vector<int> ans(2, -1);
 	
+	//to keep track of ids i created a structure/pair for song contain its id(index) and dur(duration)
 	vector<Song> songs(songDuration.size());
 	for(int i = 0; i < songDuration.size(); i++)
 	{
@@ -28,8 +29,10 @@ vector<int> findSongs(int rideduration, vector<int> songDuration)
 		songs[i].dur = songDuration[i];
 	}
 
-	sort(songs.begin(),songs.end(),[](Song a,Song b) { return a.dur < b.dur; });
+	//sorting the songs based on duration
+	sort(songs.begin(),songs.end(),[](Song a,Song b) { return a.dur < b.dur; }); 
 
+	//now finding pair which satisfy the constraints and storing songs id in ans
 	for (int i = 0; i < songDuration.size() - 1; i++)
 	{
 		int need = rideduration - 30 - songs[i].dur;
